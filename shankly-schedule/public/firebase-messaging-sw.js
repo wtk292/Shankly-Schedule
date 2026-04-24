4f793af709ec94983b21dce3bc6f7e18da374cf9
wtk292 <wtk292@gmail.com>
2026-04-24 17:32:28 -0400

    Update firebase-messaging-sw.js

--- shankly-schedule/public/firebase-messaging-sw.js
+++ shankly-schedule/public/firebase-messaging-sw.js
@@ -1,6 +1,6 @@
 importScripts('https://www.gstatic.com/firebasejs/10.8.0/firebase-app-compat.js')
 importScripts('https://www.gstatic.com/firebasejs/10.8.0/firebase-messaging-compat.js')
-
+ 
 firebase.initializeApp({
   apiKey: "AIzaSyCOao0HHSgr92kjqyvEoafEE3ozB_ygIWw",
   authDomain: "shankly-schedule.firebaseapp.com",
@@ -10,15 +10,17 @@ firebase.initializeApp({
   messagingSenderId: "939898424652",
   appId: "1:939898424652:web:e5d6923ea5b2d82c4a93e9"
 })
-
+ 
 const messaging = firebase.messaging()
-
+ 
 messaging.onBackgroundMessage(function(payload) {
   const { title, body } = payload.notification
   self.registration.showNotification(title, {
     body,
     icon: '/apple-touch-icon.png',
     badge: '/favicon-32x32.png',
-    vibrate: [200, 100, 200]
+    vibrate: [200, 100, 200],
+    tag: title + body,
+    renotify: false
   })
 })
